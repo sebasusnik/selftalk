734b863369f3c50eedfd12c6349b55076c8be7e9
sebasusnik <sebasusnik@gmail.com>
2023-06-26 20:26:49 -0300

    error 404 style

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -21,7 +21,7 @@ export default function NotFound() {
   const router = useRouter()
   return (
     <div className='container absolute inset-0 flex w-screen flex-col items-center justify-center'>
-      <section className='mx-auto mb-14 flex flex-col items-center justify-center gap-3 space-y-4 sm:w-[500px]'>
+      <section className='mx-auto mb-12 flex flex-col items-center justify-center gap-3 space-y-4 sm:w-[500px]'>
         <Badge variant={'destructive'} className=''>Error 404</Badge>
         <h2 className='scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0'>Página no encontrada</h2>
         <p className="text-center text-xl text-muted-foreground">Lo sentimos, la página que buscas no existe o ha sido removida.</p>
